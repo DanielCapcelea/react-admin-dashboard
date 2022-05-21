d63c9affe58934fb686dea93cc28e7af8af084f2
Daniel <danielcapcelea@mail.ru>
2022-05-21 20:43:35 +0200

    navbar component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import {
     Ecommerce,
     Orders,
@@ -20,11 +21,13 @@ import {
     ColorMapping,
     Editor,
 } from './pages';
+import { useStateContext } from './contexts/ContextProvider';
 
 import './App.css';
 
 const App = () => {
-    const activeMenu = true;
+    const { activeMenu } = useStateContext();
+
     return (
         <div>
             <BrowserRouter>
@@ -47,11 +50,11 @@ const App = () => {
                     </div>
                     {activeMenu ? (
                         <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
-                            Sidebar
+                            <Sidebar />
                         </div>
                     ) : (
                         <div className="w-0 dark:bg-secondary-dark-bg">
-                            Sidebar w-0
+                            <Sidebar />
                         </div>
                     )}
                     <div
@@ -60,7 +63,7 @@ const App = () => {
                         }`}
                     >
                         <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
-                            navbar
+                            <Navbar />
                         </div>
                     </div>
                     <div>
