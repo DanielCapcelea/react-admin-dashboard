a2c2ef1919816d362ce3416fbd7e84e9592c4bc6
Daniel <danielcapcelea@mail.ru>
2022-05-21 21:46:10 +0200

    complete navbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,13 @@ import { links } from '../data/mockData';
 import { useStateContext } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
-    const { activeMenu, setActiveMenu } = useStateContext();
+    const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+
+    const handleCloseSidebar = () => {
+        if (activeMenu && screenSize <= 900) {
+            setActiveMenu(false);
+        }
+    };
 
     const activeLink =
         'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
@@ -53,7 +59,7 @@ const Sidebar = () => {
                                     <NavLink
                                         to={`/${link.name}`}
                                         key={link.name}
-                                        onClick={() => {}}
+                                        onClick={handleCloseSidebar}
                                         className={({ isActive }) =>
                                             isActive ? activeLink : normalLink
                                         }
